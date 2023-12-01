64d6d9ae3bf5038aa13bbc7092a427e38a308a49
maxime-christ <contact@maximechrist.fr>
2023-12-01 11:22:17 +0100

    Part one solved.
    I should definitely learn to read what's expected of me...

--- day1/main.rb
+++ day1/main.rb
@@ -5,12 +5,8 @@ filename = ARGV[0]
 values = []
 File.open(filename, 'r') do |file|
   file.each_line do |line|
-    digits = line.strip.gsub(/[^0-9]/, '')
-    if digits.length > 1
-      values << (digits[0] + digits[-1]).to_i
-    else
-      values << digits.to_i
-    end
+    digits = line.strip.gsub(/[^1-9]/, '')
+    values << (digits[0] + digits[-1]).to_i
   end
 end
 
